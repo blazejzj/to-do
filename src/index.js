384d3c9c97970d082eb6400773ee16f53fff853d
blazejzj <opexrrator@gmail.com>
2024-06-13 20:58:40 +0200

    responsive layout (done) -> have to work more on the layout itself

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+
+// placeholder for hamburgermenu later
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector('.hamburger-menu-container');
+    const menu = document.querySelector('.menu-container');
+
+    hamburger.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
+});
