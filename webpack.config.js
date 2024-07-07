bcc6633a417b38fbccede27b9c7523377d3b8c40
blazejzj <opexrrator@gmail.com>
2024-07-07 15:05:25 +0200

    .

--- webpack.config.js
+++ webpack.config.js
@@ -16,10 +16,6 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      },
-      {
-        test: /\.svg$/,
-        use: ['file-loader']
       }
     ]
   },
